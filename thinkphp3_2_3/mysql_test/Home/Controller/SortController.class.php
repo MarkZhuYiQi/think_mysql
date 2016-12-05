f27c1803a941c2618da973c590c62f15ea6ad814
zhuyiqi_026@163.com <31415926zyq>
2016-12-05 23:06:57 +0800

    first

--- thinkphp3_2_3/mysql_test/Home/Controller/SortController.class.php
+++ thinkphp3_2_3/mysql_test/Home/Controller/SortController.class.php
@@ -21,7 +21,61 @@ class SortController extends Controller
 //        select * from news_main PARTITION(news_programsp0) order by id desc;
 
         $news=M();
-        $res=$news->query('select * from news_main PARTITION(news_programsp0) order by id desc;');
+        $class='news_';
+        $hash=0;
+        $sort='';
+        foreach($post as $item)
+        {
+            if($item->name=='classify')
+            {
+                switch($item->classId){
+                    case 1:
+                        $class.='car';
+                        break;
+                    case 2:
+                        $class.='sens';
+                        break;
+                    case 3:
+                        $class.='world';
+                        break;
+                    case 4:
+                        $class.='program';
+                        break;
+                    case 5:
+                        $class.='test';
+                        break;
+                    default:
+                        $class='';
+                }
+            }
+            else
+            {
+                if($item->classId!=0)
+                {
+                    if($item->classId>2)
+                    {
+                        $hash=$item->classId % 3;
+                    }
+                    elseif($item->classId==1)
+                    {
+                        $sort='news_click asc';
+                    }else
+                    {
+                        $sort='news_click desc';
+                    }
+                }
+                else
+                {
+                    $sort='id desc';
+                }
+            }
+        }
+        if($class!='')$where=' PARTITION('.$class.')';
+        if($hash>0)$where=' PARTITION('.$class.'sp'.$hash.') ';
+        $where.=' order by '.$sort;
+        $sql='select * from news_main '.$where;
+//        echo $sql;
+        $res=$news->query($sql);
         echo json_encode($res);
     }
 }
